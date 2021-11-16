deee863a715b52cfa84b45953b9bce3f38dba3aa
Dave <david.quinta415@gmail.com>
2021-11-16 13:43:14 -0600

    Fix title

--- pages/project-overview/index.js
+++ pages/project-overview/index.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
-import {Button} from "../../components/Button/Button.js";
+import React, { useState } from "react";
+import { Button } from "../../components/Button/Button.js";
 import Header from "../../components/Header/Header.js";
-import {Input} from "../../components/Input/Input.js";
-import {Screen} from "../../components/Screen/Screen.js";
-import {useRouter} from "next/router";
-import {PROJECTS} from "../../constants/constants.js";
+import { Input } from "../../components/Input/Input.js";
+import { Screen } from "../../components/Screen/Screen.js";
+import { useRouter } from "next/router";
+import { PROJECTS } from "../../constants/constants.js";
 import Router from "next/router";
 import swal from "sweetalert";
 import Image from "next/image";
@@ -15,7 +15,7 @@ const ProjectOverviewScreen = () => {
   const [projectDescription, setProjectDescription] = useState("");
   const [files, serFiles] = useState([]);
   const router = useRouter();
-  const {index} = router.query;
+  const { index } = router.query;
   const project = PROJECTS[parseInt(index)];
   const handleVote = () => {
     swal({
@@ -35,18 +35,13 @@ const ProjectOverviewScreen = () => {
     <Screen>
       <Header iconName="back" iconAction={() => router.back()} />
       <div className="w-full h-full bg-blue-100 px-3 pt-10">
-<<<<<<< HEAD
-        
+
+
         <span className="block text-gray-700 text-xl font-bold mb-2">{project?.title ?? ""}</span>
-=======
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Nombre del Proyecto:
-        </label>
-        <span className="text-sm">{project?.title ?? ""}</span>
->>>>>>> main
+
         <div className="h-1/2 w-full relative">
-            <Image objectFit="cover" src={project?.image ?? ""} alt="" layout="fill" />
-          </div>
+          <Image objectFit="cover" src={project?.image ?? ""} alt="" layout="fill" />
+        </div>
         <label class="block text-gray-700 text-sm font-bold mb-2 mt-5">
           Descripción del Proyecto:
         </label>
